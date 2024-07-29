609c0a24ebc0a9f6f5ddc8f74b9cfc26e461e23c
Igor Udot <47724762+horw@users.noreply.github.com>
2024-07-28 23:04:33 -0700

    feat: tabs shortcode (#68)

---
+++ assets/lib/learn/learn.js
@@ -0,0 +1,8 @@
+
+function switchTab(tabGroup, tabId) {
+    allTabItems = jQuery("[data-tab-group='"+tabGroup+"']");
+    targetTabItems = jQuery("[data-tab-group='"+tabGroup+"'][data-tab-item='"+tabId+"']");
+    
+    allTabItems.removeClass("active");
+    targetTabItems.addClass("active");
+}
